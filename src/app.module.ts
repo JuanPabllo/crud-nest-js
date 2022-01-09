5747451398321750da14aa51693cf41bd0327285
juanpabllo <juanpablo192.dreaw@gmail.com>
2022-01-09 13:53:14 -0300

    start project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
